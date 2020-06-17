a8d6c2fc540caa44a9e264961bde5a72c6b549c4
Lukasz <lukaszkkoziel@gmail.com>
2020-06-17 12:20:30 +0900

    table filter

--- HenngeChallenge/index.js
+++ HenngeChallenge/index.js
@@ -25,21 +25,20 @@ $(function() {
 });
 
 function myFunction() {
-  // Declare variables
-  var input, filter, tr, td, a, i, txtValue;
-  input = document.getElementById('myInput');
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("myInput");
   filter = input.value.toUpperCase();
-  ul = document.getElementById("myTR");
-  li = ul.getElementsByTagName('td');
-
-  // Loop through all list items, and hide those who don't match the search query
-  for (i = 0; i < li.length; i++) {
-    a = li[i].getElementsByTagName("a")[0];
-    txtValue = a.textContent || a.innerText;
-    if (txtValue.toUpperCase().indexOf(filter) > -1) {
-      li[i].style.display = "";
-    } else {
-      li[i].style.display = "none";
+  table = document.getElementById("myTable");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[3];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
     }
   }
 }
