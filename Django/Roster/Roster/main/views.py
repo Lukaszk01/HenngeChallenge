36eb89f6c21d9f4a51bdaeae10f3145b9ba39051
Lukasz <lukaszkkoziel@gmail.com>
2020-07-31 11:57:48 +0900

    routes

--- Django/Roster/Roster/main/views.py
+++ Django/Roster/Roster/main/views.py
@@ -1,12 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-  return HttpResponse('<h1>Hello world</h1>')
-    # context = {
-    #     'posts': Post.objects.all()
-    # }
-    # return render(request, 'main/home.html', context)
+    return render(request, 'main/home.html')
+
+
 def about(request):
-  return HttpResponse('<h1>Hello world</h1>')
+    return render(request, 'main/about.html', {'title': 'About'})
