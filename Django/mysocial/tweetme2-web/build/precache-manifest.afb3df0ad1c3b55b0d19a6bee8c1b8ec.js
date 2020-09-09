86d9f153440fb7cfc6d5b90b098e4440357037dd
Lukasz <lukaszkkoziel@gmail.com>
2020-09-09 11:07:58 +0900

    general changes

---
+++ Django/mysocial/tweetme2-web/build/precache-manifest.afb3df0ad1c3b55b0d19a6bee8c1b8ec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5396904423168574212d0658c464a533",
+    "url": "/index.html"
+  },
+  {
+    "revision": "77453c488f13f2548f6f",
+    "url": "/static/css/main.b32aa182.chunk.css"
+  },
+  {
+    "revision": "2c277ed9256095475955",
+    "url": "/static/js/2.ccef9922.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.ccef9922.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77453c488f13f2548f6f",
+    "url": "/static/js/main.0247f5a2.chunk.js"
+  },
+  {
+    "revision": "43cfd09573b07f1c8945",
+    "url": "/static/js/runtime-main.99d83fc2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
