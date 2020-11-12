13c865a4af60eb7b54c514cb3c23b89ccd7c2590
Lukasz <lukaszkkoziel@gmail.com>
2020-11-12 12:32:41 +0900

    conditionals

--- Vue/mastery/main.js
+++ Vue/mastery/main.js
@@ -4,5 +4,7 @@ var app = new Vue({
     product: 'Socks',
     decription: 'A pair of warm, fuzzy socks',
     image: 'https://firebasestorage.googleapis.com/v0/b/vue-mastery.appspot.com/o/flamelink%2Fmedia%2F1578366170160_4.png?alt=media&token=e0829829-627b-44e1-9d96-fa22eaca1ac9',
+    inStock: true,
+    onSale: true
   }
 })
