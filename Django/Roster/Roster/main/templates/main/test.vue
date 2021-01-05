7af5b858f150e6148e4844af0c50d828724cc06d
Lukasz <lukaszkkoziel@gmail.com>
2021-01-05 12:55:39 +0900

    test

--- Django/Roster/Roster/main/templates/main/test.vue
+++ Django/Roster/Roster/main/templates/main/test.vue
@@ -1,6 +1,13 @@
-{% extends "main/base.html" %}
-{% block content %}
+
 <script src="https://cdnjs.cloudflare.com/ajax/libs/velocity/1.2.3/velocity.min.js"></script>
 
+
+<template>
+    <input v-model="message" placeholder="edit me">
+    <p>Message is: {{ message }}</p>
+</template>
+<script>
+export default {
+    
+}
 </script>
-{{% endblock content %}}
