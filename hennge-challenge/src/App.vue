0ce33f721894a7ed3fcab74d908b09a3488fea02
Lukaszk01 <lukaszkkoziel@gmail.com>
2021-05-01 17:26:20 +0900

    changes

--- hennge-challenge/src/App.vue
+++ hennge-challenge/src/App.vue
@@ -1,57 +1,55 @@
-<template>
-  <div id="app">
+  <template>
+   <!--  <div id="app"> -->
 
-<div id="sort-bar">
+  <div id="sort-bar">
 
-<!-- 
-    <input type="text" v-model="searchValue" placeholder="Search email" id="search-input">
-    <i class="fa fa-search"></i>
+
+     <!--  <input type="text" v-model="searchValue" placeholder="Search email" id="search-input">
+      <i class="fa fa-search"></i> -->
+    </div>
+
+  <!--    <input type="date" v-model="startDate">
+     <input type="date" v-model="endDate">
+     <ul>
+       <li v-for="emails in filteredData" :key="emails.datetime">{{emails.datetime}}</li>
+     </ul>
   </div> -->
 
-   <input type="date" v-model="startDate">
-   <input type="date" v-model="endDate">
-   <ul>
-     <li v-for="datas in filteredData" :key="datas.datetime">{{datas.datetime}}</li>
-   </ul>
-</div>
-
-  <!-- Where the array of emails get rendered as cards -->
-  <div id="email-container">
-    <div class="card" v-for="datas in filteredEmails" :key="datas.subject">
-      <img :src="email.img" class="email-image">
-      <div class="content">
-        <h1 class="email-subject">
-          {{ datas.subject }}
-        </h1>
-        <p>
-          {{ data.description }}
-        </p>
-        <span class="body-span">
-
-          <h3 class="body-subject">
-            body:
-          </h3>
-          <h3>
-           <p>Recever:</p> 
-            {{ email.to }}
-          </h3>
-          <h3>
-           <p>Sender:</p> 
-            {{ email.from }}
-          </h3>
-            <h3>
-           <p>Date:</p> 
-            {{ email.datetime }}
-          </h3>
-       <!--    <ol class="body-list">
-            <li v-for="body in email.bodys">- {{ body }}</li>
-          </ol> -->
-        </span>
-      </div>
-
+    <!-- Where the array of emails get rendered as cards -->
+   <div id="sort-bar">
+      <input type="text" v-model="searchValue" placeholder="Search Recipe" id="search-input">
+      <i class="fa fa-search"></i>
     </div>
-  </div>
-  </div>
+      
+    <!-- Where the array of recipes get rendered as cards -->
+    <div id="recipe-container">
+      <div class="card" v-for="email in filteredEmails" :key="email.subject">
+        <div class="content">
+          <h1 class="email-title">
+            {{ email.subject }}
+          </h1>
+          <p>
+            {{ email.body }}
+          </p>
+          <span class="ingredient-span">
+
+            <h3 class="ingredient-title">
+              Ingredients:
+            </h3>
+            
+          </span>
+
+          <h3>
+            Cooking Time:
+          </h3>
+          <p>
+            {{email.cookingTime}} minutes
+          </p>
+
+        </div>
+
+      </div>
+    </div>
 
 
 
@@ -64,200 +62,199 @@
 
 
 
+      <h2>emails</h2>
+      <input type="text" v-model="searchValue" placeholder="Search Email" id="search-input">
 
-    <h2>emails</h2>
-    <input type="text" v-model="searchValue" placeholder="Search Email" id="search-input">
 
+      <div class="emails">
+      <EmailData v-for="email in emails" :key="email.id" :email="email" />
+    </div>
+  </template>
 
-    <div class="emails">
-    <EmailData v-for="email in emails" :key="email.id" :email="email" />
-  </div>
-</template>
+  <script>
+  import EmailData from '@/components/EmailData.vue'
+  // import _ from 'lodash';
 
-<script>
-import EmailData from '@/components/EmailData.vue'
-import _ from 'lodash';
-
-export default {
-  name: 'Home',
-  components: {
-    EmailData
-  },
-  data() {
-    return {
-      startDate: null,
-      endDate: null,
-      ascending: true,
-      sortBy: 'alphabetically',
-      searchValue: '',
-      datas: [
-        { 
-          id: 1,
-          from: "aaa1@example.com",
-          to: "zzzz.zzzz@example.com",
-          subject: "[HR-8888] Noticed of official announcement",
-          attachment: "",
-          datetime: "2021-11-24T15:15:00+0200",
-          body: "this is a fake body"
-        },
-        {
-          id: 2,
-          from: "bbb1@example.com",
-          to: "zzzz.zzzz@example.com",
-          subject: "[Github] Logout page",
-          attachment: "",
-          datetime: "2021-11-25T15:15:00+0200",
-          body: "this is a fake body"
-        },
-        {
-          id: 3,
-          from: "ccc1@example.com",
-          to: "zzzz.zzzz@example.com",
-          subject: "[dev]Postfix 3.1.12 / 3.2.9 / 3.3.4 / 3.4.5",
-          attachment: "",
-          datetime: "2021-11-26T15:15:00+0200",
-          body: "this is a fake body"
-        }
-        ,
-        {
-          id: 4,
-          from: "ddd1@example.com",
-          to: "mmmm.zzzz@example.com",
-          subject: "[HR-8888] Noticed of official announcement",
-          attachment: "",
-          datetime: "2021-10-16T15:15:00+0200",
-          body: "this is a fake body"
-        }
-        ,
-        {
-          id: 5,
-          from: "eee1@example.com",
-          to: "tttt.zzzz@example.com",
-          subject: "[dev]Postfix 3.1.12 / 3.2.9 / 3.3.4 / 3.4.5",
-          attachment: "",
-          datetime: "2021-10-10T15:15:00+0200",
-          body: "this is a fake body"
-        }
-        ,
-        {
-          id: 6,
-          from: "fff1@example.com",
-          to: "pppp.zzzz@example.com",
-          subject: "[HR-8888] Noticed of official announcement",
-          attachment: "",
-          datetime: "2021-11-30T15:15:00+0200",
-          body: "this is a fake body"
-        }
-        ,
-        {
-          id: 7,
-          from: "ggg1@example.com",
-          to: "ooo.zzzz@example.com",
-          subject: "[dev]Postfix 3.1.12 / 3.2.9 / 3.3.4 / 3.4.5",
-          attachment: "",
-          datetime: "2020-11-24T15:15:00+0200",
-          body: "this is a fake body"
-        }
-        ,
-        {
-          id: 8,
-          from: "hhh1@example.com",
-          to: "tttt.zzzz@example.com",
-          subject: "[HR-8888] Noticed of official announcement",
-          attachment: "",
-          datetime: "2021-03-21T15:15:00+0200",
-          body: "this is a fake body"
-        }
-        ,
-        {
-          id: 9,
-          from: "iii1@example.com",
-          to: "xxx.zzzz@example.com",
-          subject: "[dev]Postfix 3.1.12 / 3.2.9 / 3.3.4 / 3.4.5",
-          attachment: "",
-          datetime: "2021-06-17T15:15:00+0200",
-          body: "this is a fake body"
-        }
-        ,
-        {
-          id: 10,
-          from: "jjj1@example.com",
-          to: "zzzz.zzzz@example.com",
-          subject: "[HR-8888] Noticed of official announcement",
-          attachment: "",
-          datetime: "2021-11-24T15:15:00+0200",
-          body: "this is a fake body"
-        }
-    ]
-  ,
-  computed: {
-    filteredData(){
-      var vm = this
-      var startDate = vm.startDate;
-      var endDate = vm.endDate;
-      return _.filter(vm.data, (function (data) {
-        if ((_.isNull(startDate) && _.isNull(endDate))) {
-          return true
-        } else {
-          var date = data;
-          return (date >= startDate && date <= endDate);
-        }
-      }))
-    }
-  },
+  export default {
+    name: 'Home',
+    components: {
+      EmailData
+    },
+    data() {
+      return {
+        startDate: null,
+        endDate: null,
+        ascending: true,
+        sortBy: 'alphabetically',
+        searchValue: '',
+        emails: [
+          { 
+            id: 1,
+            from: "aaa1@example.com",
+            to: "zzzz.zzzz@example.com",
+            subject: "[HR-8888] Noticed of official announcement",
+            attachment: "",
+            datetime: "2021-11-24T15:15:00+0200",
+            body: "this is a fake body"
+          },
+          {
+            id: 2,
+            from: "bbb1@example.com",
+            to: "zzzz.zzzz@example.com",
+            subject: "[Github] Logout page",
+            attachment: "",
+            datetime: "2021-11-25T15:15:00+0200",
+            body: "this is a fake body"
+          },
+          {
+            id: 3,
+            from: "ccc1@example.com",
+            to: "zzzz.zzzz@example.com",
+            subject: "[dev]Postfix 3.1.12 / 3.2.9 / 3.3.4 / 3.4.5",
+            attachment: "",
+            datetime: "2021-11-26T15:15:00+0200",
+            body: "this is a fake body"
+          }
+          ,
+          {
+            id: 4,
+            from: "ddd1@example.com",
+            to: "mmmm.zzzz@example.com",
+            subject: "[HR-8888] Noticed of official announcement",
+            attachment: "",
+            datetime: "2021-10-16T15:15:00+0200",
+            body: "this is a fake body"
+          }
+          ,
+          {
+            id: 5,
+            from: "eee1@example.com",
+            to: "tttt.zzzz@example.com",
+            subject: "[dev]Postfix 3.1.12 / 3.2.9 / 3.3.4 / 3.4.5",
+            attachment: "",
+            datetime: "2021-10-10T15:15:00+0200",
+            body: "this is a fake body"
+          }
+          ,
+          {
+            id: 6,
+            from: "fff1@example.com",
+            to: "pppp.zzzz@example.com",
+            subject: "[HR-8888] Noticed of official announcement",
+            attachment: "",
+            datetime: "2021-11-30T15:15:00+0200",
+            body: "this is a fake body"
+          }
+          ,
+          {
+            id: 7,
+            from: "ggg1@example.com",
+            to: "ooo.zzzz@example.com",
+            subject: "[dev]Postfix 3.1.12 / 3.2.9 / 3.3.4 / 3.4.5",
+            attachment: "",
+            datetime: "2020-11-24T15:15:00+0200",
+            body: "this is a fake body"
+          }
+          ,
+          {
+            id: 8,
+            from: "hhh1@example.com",
+            to: "tttt.zzzz@example.com",
+            subject: "[HR-8888] Noticed of official announcement",
+            attachment: "",
+            datetime: "2021-03-21T15:15:00+0200",
+            body: "this is a fake body"
+          }
+          ,
+          {
+            id: 9,
+            from: "iii1@example.com",
+            to: "xxx.zzzz@example.com",
+            subject: "[dev]Postfix 3.1.12 / 3.2.9 / 3.3.4 / 3.4.5",
+            attachment: "",
+            datetime: "2021-06-17T15:15:00+0200",
+            body: "this is a fake body"
+          }
+          ,
+          {
+            id: 10,
+            from: "jjj1@example.com",
+            to: "zzzz.zzzz@example.com",
+            subject: "[HR-8888] Noticed of official announcement",
+            attachment: "",
+            datetime: "2021-11-24T15:15:00+0200",
+            body: "this is a fake body"
+          }
+      ]
+    ,
+    computed: {
+      // filteredData(){
+      //   var vm = this
+      //   var startDate = vm.startDate;
+      //   var endDate = vm.endDate;
+      //   return _.filter(vm.emails, (function (emails) {
+      //     if ((_.isNull(startDate) && _.isNull(endDate))) {
+      //       return true
+      //     } else {
+      //       var date = emails;
+      //       return (date >= startDate && date <= endDate);
+      //     }
+      //   }))
+      // }
+    },
     filteredEmails() {
-    let tempEmails = this
-    
-    // Process search input
-    if (this.searchValue != '' && this.searchValue) {
-        tempEmails = tempEmails.filter((item) => {
-          return item.datetime
-            .toUpperCase()
-            .includes(this.searchValue.toUpperCase())
-        })
-      }
-    
-    // Sort by alphabetical order
-        tempEmails = tempEmails.sort((a, b) => {
-            if (this.sortBy == 'alphabetically') {
-                let fa = a.datetime.toLowerCase(), fb = b.datetime.toLowerCase()
+      let tempEmails = this.emails
+      
+      // Process search input
+      if (this.searchValue != '' && this.searchValue) {
+          tempEmails = tempEmails.filter((item) => {
+            return item.title
+              .toUpperCase()
+              .includes(this.searchValue.toUpperCase())
+          })
+        }
+             
+      // Sort by alphabetical order
+          tempEmails = tempEmails.sort((a, b) => {
+              if (this.sortBy == 'alphabetically') {
+                  let fa = a.title.toLowerCase(), fb = b.title.toLowerCase()
+            
+                if (fa < fb) {
+                  return -1
+                }
+                if (fa > fb) {
+                  return 1 
+                }
+                return 0
+          }
+          })
           
-              if (fa < fb) {
-                return -1
-              }
-              if (fa > fb) {
-                return 1 
-              }
-              return 0
+          // Show sorted array in descending or ascending order
+          if (!this.ascending) {
+            tempEmails.reverse()
+          }
+          
+          return tempEmails
         }
-        })
-        
-        // Show sorted array in descending or ascending order
-        if (!this.ascending) {
-          tempEmails.reverse()
-        }
-        
-        return tempEmails
-  }
+      }
     }
+  };
+  </script>
+
+  <style>
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
   }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  </style>
 
 
 
 
-export default {
-  
-};
+  export default {
+    
+  };
